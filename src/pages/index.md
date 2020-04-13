22808051627a0833cf06062c2b2b3cbf3f29e840
Achim Ismaili <achim@ismaili.de>
2020-04-13 17:04:34 +0200

    Startseite index.md refactored

--- src/pages/index.md
+++ src/pages/index.md
@@ -9,6 +9,7 @@ mainpitch:
     Unser Ziel ist es, Dich bei Deinen sportlichen Zielen zu unterstützen und
     sowohl Gemeinschaft als auch Kultur zu fördern. Wir laden Dich ein, ein Teil
     von uns zu werden.
+heading: Was bieten wir?
 description: >-
   Kaldi is the ultimate spot for coffee lovers who want to learn about their
   java’s origin and support the farmers that grew it. We take coffee production,
@@ -43,12 +44,6 @@ intro:
         space where you can hang out with fellow coffee lovers and learn about
         coffee making techniques. All of the artwork on display there is for
         sale. The full price you pay goes to the artist.
-  heading: What we offer
-  description: >
-    Kaldi is the ultimate spot for coffee lovers who want to learn about their
-    java’s origin and support the farmers that grew it. We take coffee
-    production, roasting and brewing seriously and we’re glad to pass that
-    knowledge to anyone. This is an edit via identity...
 main:
   heading: tbd
   description: >
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -78,6 +78,17 @@ export const IndexPageTemplate = ({
                     <h3 className="subtitle">{mainpitch.description}</h3>
                   </div>
                 </div>
+                <div className="column is-12">
+                  <h3 className="has-text-weight-semibold is-size-2">
+                    Aktuelles
+                  </h3>
+                  <BlogRoll />
+                  <div className="column is-12 has-text-centered">
+                    <Link className="btn" to="/blog">
+                      Alle News & Events
+                    </Link>
+                  </div>
+                </div>
                 <div className="columns">
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
@@ -89,19 +100,8 @@ export const IndexPageTemplate = ({
                 <Features gridItems={intro.blurbs} />
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
+                    <Link className="btn" to="/verein">
+                      Mehr zum Verein
                     </Link>
                   </div>
                 </div>
