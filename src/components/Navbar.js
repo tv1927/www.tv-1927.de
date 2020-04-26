4ef4a8089e452a36ba0b76a226d23fab8fcde73d
Achim Ismaili <achim@ismaili.de>
2020-04-26 19:07:29 +0200

    Blog link went behind club

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,11 +59,11 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
               <Link className="navbar-item" to="/verein">
                 Dein Verein
+              </Link>
+              <Link className="navbar-item" to="/blog">
+                Blog
               </Link>
               <Link className="navbar-item" to="/kontakt">
                 Kontakt
