8bc14f61d691e31037d1b06c75dcc23633c9e29b
Achim Ismaili <achim@ismaili.de>
2020-08-07 22:21:55 +0200

    Upgraded netlify cms

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+// import {
+//   FBComments,
+//   FBCommentsCount,
+//   FBEmbedPost,
+//   FBEmbedVideo,
+//   FBFollow,
+//   FBLike,
+//   FBPage,
+//   FBSend,
+//   FBShare
+// } from 'facebook-plugins';
 
 import AreaFeatures from '../components/AreaFeatures'
 import Layout from '../components/Layout'
-import Features from '../components/Features'
 import BlogNews from '../components/BlogNews'
 
 export const IndexPageTemplate = ({
@@ -88,6 +98,11 @@ export const IndexPageTemplate = ({
                       {heading}
                     </h3>
                     <p>{description}</p>
+                    {/* <div className="columns">
+                  <FBPage appId="821262015065038"
+                    href="https://www.facebook.com/facebook"
+                    tabs={['timeline', 'events', 'messages']}/>
+                </div> */}
                       <AreaFeatures />
                   </div>
                 </div>
